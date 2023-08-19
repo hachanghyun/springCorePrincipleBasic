460f6f0e1a5b36db6845b918c0b182cd665d7e60
hachanghyun <haccwx@naver.com>
2023-08-19 21:38:07 +0900

    예제 commit

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -24,4 +24,9 @@ public class OrderServiceImpl implements OrderService {
 
         return new Order(memberId, itemNane, itemPrice, discountPrice);
     }
+
+    //테스트 용도
+    public MemberRepository getMemberRepository() {
+        return memberRepository;
+    }
 }
